23094063b48f3c61b49f460652411382f92b0731
Muhammad Ali <un1qu31d@gmail.com>
2024-11-14 15:33:12 +0200

    Setup Fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -40,9 +40,12 @@ const config: Config = {
       screens: {
         xs: "420px",
       },
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        "space-grotesk": ["var(--font-space-grotesk)"],
+      },
     },
   },
   plugins: [],
 };
-
 export default config;
